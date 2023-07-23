e3ee0367c1fd7d1377cf2981f6cda8db58589477
Andrés Francisco almarza menjiba <francisco.almarza@gmail.com>
2023-07-23 09:46:17 -0400

    ajuste de datos

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// se agrega como base el repo-name
 export default defineConfig({
+  base: '/transportes/',
   plugins: [react()],
 })
