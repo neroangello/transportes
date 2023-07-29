66ff350c263e43a0d76942aa67e82dffbff3a904
Andrés Francisco almarza menjiba <francisco.almarza@gmail.com>
2023-07-29 09:03:39 -0400

    mejoras, control de errores y ajustes

--- src/components/Busqueda/index.jsx
+++ src/components/Busqueda/index.jsx
@@ -3,6 +3,11 @@ import axios from "axios";
 import './busqueda.scss';
 
 import { If, Then, ElseIf, Else } from 'react-if-elseif-else-render';
+import imgParadero from './imgs/bus-stop.png';
+import imgBus from './imgs/bus.png';
+import imgEmail from './imgs/email.png';
+import imgSms from './imgs/sms.png';
+import imgNotify from './imgs/notification.png';
 
 
 class Informacion{
@@ -84,96 +89,128 @@ export const Busqueda = () => {
 
     const [value, setValue] = useState('');
     const [resultados, setResultados] = useState([]);
+    const [paradero, setParadero] = useState([]);
 
 
     function handleClick() {
-        axios.get("https://api.xor.cl/red/bus-stop/" + value).then((response) => {
 
-        const resultado = response.data;
-        
-        const informaciones = [];
+ 
+            axios.get("https://api.xor.cl/red/bus-stop/" + value).then((response) => {
 
-        response.data.services.forEach(srv => {
-            
-            //const buses = [];            
+                const resultado = response.data;
+                
+                const informaciones = [];
+                const encabezado = []
+                let elemento = 0;
 
-            srv.buses.forEach(micro => {
+                response.data.services.forEach(srv => {
 
-                const info = new Informacion(
-                    resultado.id, 
-                    resultado.name, 
-                    resultado.status_code, 
-                    resultado.status_description,
-                    srv.id, 
-                    srv.valid, 
-                    srv.status_description,
-                    micro.id, 
-                    micro.meters_distance, 
-                    micro.min_arrival_time, 
-                    micro.max_arrival_time
-                );
+                    srv.buses.forEach(micro => {
+
+                        const info = new Informacion(
+                            resultado.id, 
+                            resultado.name, 
+                            resultado.status_code, 
+                            resultado.status_description,
+                            srv.id, 
+                            srv.valid, 
+                            srv.status_description,
+                            micro.id, 
+                            micro.meters_distance, 
+                            micro.min_arrival_time, 
+                            micro.max_arrival_time
+                        );
 
 
-                //const microbus = new Bus(micro.id, micro.meters_distance, micro.min_arrival_time, micro.max_arrival_time);
-                //buses.push(microbus);
+                        //const microbus = new Bus(micro.id, micro.meters_distance, micro.min_arrival_time, micro.max_arrival_time);
+                        //buses.push(microbus);
 
-                informaciones.push(info);
+                        informaciones.push(info);
+                        if(elemento == 0){
+                            encabezado.push(info);
+                            console.log(info);
+                        }
+                        elemento++;
 
-            });
-            //const serv = new Servicio(srv.id, srv.valid, srv.status_description, buses);
-            //servicios.push(serv);
-            
-        });
+                    });
+                    //const serv = new Servicio(srv.id, srv.valid, srv.status_description, buses);
+                    //servicios.push(serv);
+                    
+                });
 
-        //const paradero = new Paradero(resultado.id, resultado.name, resultado.status_code, resultado.status_description, servicios);
+                //const paradero = new Paradero(resultado.id, resultado.name, resultado.status_code, resultado.status_description, servicios);
 
-        console.log(informaciones);
-        setResultados(informaciones);
+                console.log(informaciones);
+                setResultados(informaciones);
+                setParadero(encabezado);
 
-        }, [value]);
+            }, [value])
+            .catch(
+                error => {
+                    const informaciones = [];
+                    console.log(informaciones);
+                    setResultados(informaciones);
+                    console.log("Error detectado: " + error)
+                 }
+            );
+
+
       }
 
     return (
         <div>
-            <div>
-                <input type="text" placeholder="Ingrese paradero" 
-                value={value} onChange={(e) => {
-                    setValue(e.target.value);
-                }}
-                />
-                <button onClick={handleClick}>Buscar Paradero</button>
+            <h3>Locomoción disponible por paradero</h3>
+            <div className="div-busqueda">
+                <div><input className="campo-busqueda" type="text" placeholder="Ingrese paradero (Ejemplo: PA443)" 
+                    value={value} onChange={(e) => {
+                        setValue(e.target.value);
+                    }}
+                    required
+                    />
+                </div>
+                <div>
+                    <button className="button" onClick={handleClick}>Buscar Paradero</button>
+                </div>
             </div>
-            <div>       
-                       
+            <div>      
                 <If condition={(Object.keys(resultados).length != 0) }>
                     <Then>
-                        <table>
+                        {paradero.map((par) => (
+                            <div className="datos-paradero" key={par.busId}>
+                                <img className="img-icono" src={imgParadero} ></img>
+                                <span>Paradero: {par.paradId} : {par.paradDesc}</span>
+                            </div>                           
+                                                      
+                        ))}
+                        <table className="tabla-recorridos">
                             <thead>
                                 <tr>
+                                    <th></th>
                                     <th>Línea</th>
                                     <th>Bus</th>
                                     <th>Distancia (Mts.)</th>
-                                    <th>Tpo. llegada Mínimo (Minutos)</th>
-                                    <th>Tpo. llegada Máximo (Minutos)</th>
-                                    <th>Acciones</th>
+                                    <th>Tiempo llegada (Mins) </th>
+                                    <th>Notificación Diaria</th>
                                 </tr>
-                            </thead>
+                            </thead>                            
                             <tbody>
                             {resultados.map((serv) => (
                                 <tr key={serv.busId}>
+                                    <td><img className="img-bus" src={imgBus}></img></td>
                                     <td>{serv.servId}</td>
                                     <td>{serv.busId}</td>
                                     <td>{serv.busDistancia}</td>
-                                    <td>{serv.busTpoMin}</td>
-                                    <td>{serv.busTpoMax}</td>
-                                    <td>PEND</td>
+                                    <td>{serv.busTpoMin} | {serv.busTpoMax}</td>
+                                    <td><img className="img-action" src={imgEmail} title="Activar notificación por email."></img>
+                                        <img className="img-action" src={imgSms} title="Activar notificación por sms."></img>
+                                        <img className="img-action" src={imgNotify} title="Activar notificaciones en la aplicación."></img></td>
                                 </tr>    
                             ))}
                             </tbody>
                         </table>
                     </Then>
                     <Else>
-                        <table>
+                        <table className="tabla-recorridos">
                             <thead>
                                 <tr>
                                     <td>Detalle de búsqueda</td>
@@ -181,7 +218,7 @@ export const Busqueda = () => {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>No hay datos</td>
+                                    <td>Ingrese un paradero para obtener su información.</td>
                                 </tr>
                             </tbody>    
                         </table>    
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
 
     return(
         <header >
-            Haeder
+            Header
         </header>
     )
 
